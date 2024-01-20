d6b35598573ea6d959148e01a4a04f7680fc118d
Vansh Vasishtha <vansh19092002@gmail.com>
2024-01-20 11:08:00 +0530

    Sidebar Right Changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,59 +1,58 @@
-"use client";
-
+"use client"
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import { twMerge } from "tailwind-merge";
 import { usePathname } from "next/navigation";
-
-
-
 import SidebarItem from "./SidebarItem";
 import Box from "./Box";
 import Library from "./Library";
 import { useMemo } from "react";
 
-
 interface SidebarProps {
   children: React.ReactNode;
-
 }
 
 const Sidebar = ({ children }: SidebarProps) => {
   const pathname = usePathname();
 
-
-  const routes = useMemo(() => [
-    {
-      icon: HiHome,
-      label: 'Home',
-      active: pathname !== '/search',
-      href: '/'
-    },
-    {
-      icon: BiSearch,
-      label: 'Search',
-      href: '/search',
-      active: pathname === '/search'
-    },
-  ], [pathname]);
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        href: "/search",
+        active: pathname === "/search",
+      },
+    ],
+    [pathname]
+  );
 
   return (
-    <div 
-      className={twMerge(`flex h-full 
-        `
-      )}
+    <div
+      className={twMerge(`
+        flex 
+        h-full
+      `)}
     >
-      <div 
-        className="
-        hidden 
-        md:flex 
-        flex-col 
-        gap-y-2 
-        bg-black 
-        h-full 
-        w-[300px] 
-        p-2
-        "
+      <div
+        className={twMerge(`
+          hidden 
+          md:flex 
+          flex-col 
+          gap-y-2 
+          bg-black 
+          h-full 
+          w-[300px] 
+          p-2
+          fixed
+          right-0 // Move sidebar to the right
+        `)}
       >
         <Box>
           <div className="flex flex-col gap-y-4 px-5 py-4">
@@ -63,11 +62,11 @@ const Sidebar = ({ children }: SidebarProps) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-        <Library/>
+          <Library />
         </Box>
       </div>
     </div>
   );
-}
- 
+};
+
 export default Sidebar;
